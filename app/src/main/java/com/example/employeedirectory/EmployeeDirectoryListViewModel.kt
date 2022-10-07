a8e747984ce48cdaeded8786462974c49d6ab898
Goce <ganastasovski@alarm.com>
2022-10-06 21:42:23 -0400

    ganastasovski got employees from server and show them in a recycler view

--- app/src/main/java/com/example/employeedirectory/EmployeeDirectoryListViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.employeedirectory
-
-import androidx.lifecycle.ViewModel
-
-class EmployeeDirectoryListViewModel: ViewModel() {
-}
---
+++ app/src/main/java/com/example/employeedirectory/presentation/EmployeeDirectoryListAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.employeedirectory.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.employeedirectory.R
+import com.example.employeedirectory.data.Employee
+import com.example.employeedirectory.databinding.EmployeeItemListBinding
+
+class EmployeeDirectoryViewHolder(
+    private val binding: EmployeeItemListBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(employee: Employee) {
+        binding.name.text = employee.fullName
+        binding.team.text = employee.team
+        binding.photo.load(employee.photoUrlSmall) {
+            placeholder(R.drawable.place_holder_image_homer_simpson)
+        }
+    }
+}
+
+class EmployeeDirectoryListAdapter(
+    private val employees: List<Employee>
+) : RecyclerView.Adapter<EmployeeDirectoryViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeDirectoryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = EmployeeItemListBinding.inflate(inflater, parent, false)
+        return EmployeeDirectoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: EmployeeDirectoryViewHolder, position: Int) {
+        holder.bind(employees[position])
+    }
+
+    override fun getItemCount(): Int {
+        return employees.size
+    }
+}
