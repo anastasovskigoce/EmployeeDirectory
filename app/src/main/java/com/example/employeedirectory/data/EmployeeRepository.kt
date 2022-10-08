90091d908126be8f546d6b7c00c176ce69e30682
Goce <ganastasovski@alarm.com>
2022-10-08 18:04:38 -0400

    ganastasovski added build config url

--- app/src/main/java/com/example/employeedirectory/data/EmployeeRepository.kt
+++ app/src/main/java/com/example/employeedirectory/data/EmployeeRepository.kt
@@ -1,12 +1,11 @@
 package com.example.employeedirectory.data
 
+import com.example.employeedirectory.BuildConfig
 import com.example.employeedirectory.EmployeeApi
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
-
-private const val BASE_URL = "https://s3.amazonaws.com/sq-mobile-interview/"
 
 class EmployeeRepository {
     private val employeeApi: EmployeeApi
@@ -16,7 +15,7 @@ class EmployeeRepository {
             .build()
 
         val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BuildConfig.API_BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
             .client(okHttpClient)
             .build()
